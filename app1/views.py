713aee904ef843e3d6d4f453b77f7d78d068ddae
Sonali Misra <sonalimisra2001@gmail.com>
2024-02-19 18:25:43 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def deepak(request):
+    return render(request,'deepak.html')
---
+++ app2/urls.py
@@ -0,0 +1,8 @@
+from app2.views import *
+from django.urls import path
+
+app_name='kn karucha'
+
+urlpatterns=[
+    path('deepu/',deepu,name='deepu')
+]
